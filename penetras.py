fb2aa6f447452304363eca101ea631511c28b123
renan dias <renanrdias@gmail.com>
2023-07-30 14:21:08 -0300

    Adjusted gerar_penetras_dict fuction

--- penetras.py
+++ penetras.py
@@ -1,5 +1,11 @@
 from typing import List
+from funcionarios_final import *
+import configparser
+import glob
 
+config = configparser.ConfigParser()
+config.read_file(open("dl.cfg"))
+BASE_PATH = config["BASE_PATH"]["ROOT"]
 
 def gerar_penetras_dict(lista_presentes:List[List], final_report:dict,) -> dict:
     """Reads in a 2-dimensional list and convert it to a dict.
@@ -10,21 +16,27 @@ def gerar_penetras_dict(lista_presentes:List[List], final_report:dict,) -> dict:
     Return:
         dict
     """
-
+    
+    # Excluir Histórico de Promoção de cada lista
+    lista_presentes = [l[:3] for l in lista_presentes]
     # colunas = lista_presentes[0][1:]
-    festa_dict = {k: v for k, v in zip([idx[0] for idx in lista_presentes[1:]], \
-                                       [*map(dict, [[*zip(lista_presentes[0][1:], entrada[1:])]\
-                                                     for entrada in lista_presentes[1:]])])}
+    festa_dict = {e[0]: dict(*map(dict,[zip(lista_presentes[0][1:], e[1:])])) for e in lista_presentes[1:]}
     
     penetra_dict = {e: festa_dict[e] for e in festa_dict if (e not in final_report) or (e in final_report and festa_dict[e]["Nome"] != final_report[e]["Nome"])}
     return penetra_dict
 
 
 
+
+
 if __name__ == "__main__":
-    pass
-
-
-
-    
-    
+    folders = os.listdir(os.path.realpath("./"))
+    years = [year for year in folders if year.startswith("20")]
+    penetras = glob.glob("*/penetras_festa_*.json")
+    print(penetras)
+    penetras_total_dict = {}
+    for p in penetras:
+        if int(p.split("/")[0]) == 2022:
+            penetras_total_dict.update(ler_json_file(int(p.split("/")[0]), p.split("/")[1]))
+    print(penetras_total_dict)    
+    # pass
